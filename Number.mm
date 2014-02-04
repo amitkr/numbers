311c263925465ea9ac5a16cc084453c4a44e12ce
Amit Kumar <amitkr@rocketmail.com>
2014-02-05 00:30:22 +1100

    memory map

---
+++ Number.mm
@@ -0,0 +1,286 @@
+<map version="0.9.0">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node COLOR="#000000" CREATED="1301802221898" ID="ID_1058489774" MODIFIED="1390803254375" STYLE="fork">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Number
+    </p>
+  </body>
+</html></richcontent>
+<font NAME="SansSerif" SIZE="20"/>
+<hook NAME="accessories/plugins/AutomaticLayout.properties"/>
+<node COLOR="#0033ff" CREATED="1301802249609" ID="ID_127966128" MODIFIED="1390803183600" POSITION="right" TEXT="is a" VSHIFT="21">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1301802303221" HGAP="21" ID="ID_644985633" MODIFIED="1390803183600" TEXT="mathematical object" VSHIFT="-22">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1301803457058" ID="ID_79386000" MODIFIED="1390803183601" TEXT="abstract">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1301802336644" HGAP="19" ID="ID_1831662821" MODIFIED="1390803183601" POSITION="left" TEXT="used to" VSHIFT="-19">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1301802355443" HGAP="25" ID="ID_1838301933" MODIFIED="1390803183601" TEXT="count">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1301802361891" HGAP="25" ID="ID_1097149315" MODIFIED="1390803183601" TEXT="measure" VSHIFT="5">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1301802518473" ID="ID_1313623237" MODIFIED="1390803183601" POSITION="right" TEXT="represented by" VSHIFT="10">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1301802534833" HGAP="56" ID="ID_1530094790" MODIFIED="1390803183601" TEXT="notational symbol" VGAP="2" VSHIFT="15">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1301802544225" HGAP="110" ID="ID_1529053693" MODIFIED="1390803183601" TEXT="numerals" VGAP="0" VSHIFT="-17">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1301804362109" ID="ID_353140337" MODIFIED="1390803183601" TEXT="Cardinal">
+<font NAME="SansSerif" SIZE="14"/>
+<node COLOR="#111111" CREATED="1301804473093" ID="ID_790278031" MODIFIED="1390803183601" TEXT="describe">
+<node COLOR="#111111" CREATED="1301804481043" ID="ID_255335689" MODIFIED="1390803183601" TEXT="quantity">
+<node COLOR="#111111" CREATED="1301804489358" ID="ID_1407958967" MODIFIED="1390803183601" TEXT="example">
+<node COLOR="#111111" CREATED="1301804492768" ID="ID_655089142" MODIFIED="1390803183601" TEXT="one, two, three"/>
+</node>
+</node>
+</node>
+</node>
+<node COLOR="#990000" CREATED="1301804386043" ID="ID_1984841047" MODIFIED="1390803183602" TEXT="Ordinal">
+<font NAME="SansSerif" SIZE="14"/>
+<node COLOR="#111111" CREATED="1301804517746" ID="ID_8550908" MODIFIED="1390803183602" TEXT="describe">
+<node COLOR="#111111" CREATED="1301804527069" ID="ID_1967410437" MODIFIED="1390803183602" TEXT="position">
+<node COLOR="#111111" CREATED="1301804532412" ID="ID_869932987" MODIFIED="1390803183602" TEXT="example">
+<node COLOR="#111111" CREATED="1301804538124" ID="ID_218332843" MODIFIED="1390803183602" TEXT="first, second, last, next, previous"/>
+</node>
+</node>
+</node>
+<node COLOR="#111111" CREATED="1301804697316" ID="ID_679308155" MODIFIED="1390803183602" TEXT="Ranking">
+<node COLOR="#111111" CREATED="1301804704968" ID="ID_364768322" MODIFIED="1390803183602" TEXT="describe">
+<node COLOR="#111111" CREATED="1301804712767" ID="ID_1099482598" MODIFIED="1390803183602" TEXT="order/relevance/importance">
+<node COLOR="#111111" CREATED="1301804736971" ID="ID_819695388" MODIFIED="1390803183602" TEXT="example">
+<node COLOR="#111111" CREATED="1301804739680" ID="ID_1739937640" MODIFIED="1390803183602" TEXT="primary, secondary, tertiary"/>
+</node>
+</node>
+</node>
+</node>
+</node>
+<node COLOR="#990000" CREATED="1301804390487" ID="ID_1059422447" MODIFIED="1390803183602" TEXT="Partitive">
+<font NAME="SansSerif" SIZE="14"/>
+<node COLOR="#111111" CREATED="1301804590313" ID="ID_1878334445" MODIFIED="1390803183602" TEXT="describe">
+<node COLOR="#111111" CREATED="1301804595442" ID="ID_827850428" MODIFIED="1390803183603" TEXT="division">
+<node COLOR="#111111" CREATED="1301804598795" ID="ID_1864276508" MODIFIED="1390803183603" TEXT="example">
+<node COLOR="#111111" CREATED="1301804622525" ID="ID_1330377886" MODIFIED="1390803183603" TEXT="half, one-third, quarter"/>
+</node>
+</node>
+</node>
+<node COLOR="#111111" CREATED="1301804773295" ID="ID_1758552081" MODIFIED="1390803183603" TEXT="Composite">
+<node COLOR="#111111" CREATED="1301804797866" ID="ID_420236519" MODIFIED="1390803183603" TEXT="describe">
+<node COLOR="#111111" CREATED="1301804803055" ID="ID_701170622" MODIFIED="1390803183603" TEXT="composition">
+<node COLOR="#111111" CREATED="1301804807188" ID="ID_1615938655" MODIFIED="1390803183603" TEXT="example">
+<node COLOR="#111111" CREATED="1301804809517" ID="ID_976333218" MODIFIED="1390803183603" TEXT="unary, binary, ternary"/>
+</node>
+</node>
+</node>
+</node>
+</node>
+<node COLOR="#990000" CREATED="1301804396662" ID="ID_393046184" MODIFIED="1390803183603" TEXT="Multiplicative">
+<font NAME="SansSerif" SIZE="14"/>
+<node COLOR="#111111" CREATED="1301804866417" ID="ID_620377097" MODIFIED="1390803183603" TEXT="describe">
+<node COLOR="#111111" CREATED="1301804870932" ID="ID_1917232644" MODIFIED="1390803183603" TEXT="repetition">
+<node COLOR="#111111" CREATED="1301804880212" ID="ID_570340184" MODIFIED="1390803183603" TEXT="example">
+<node COLOR="#111111" CREATED="1301804883237" ID="ID_306102128" MODIFIED="1390803183604" TEXT="once, twice, thrice"/>
+</node>
+</node>
+</node>
+<node COLOR="#111111" CREATED="1301804914137" ID="ID_662907043" MODIFIED="1390803183604" TEXT="Replication">
+<node COLOR="#111111" CREATED="1301804919984" ID="ID_611589837" MODIFIED="1390803183604" TEXT="describe">
+<node COLOR="#111111" CREATED="1301804924675" ID="ID_693376270" MODIFIED="1390803183604" TEXT="replication">
+<node COLOR="#111111" CREATED="1301804927967" ID="ID_1246419600" MODIFIED="1390803183604" TEXT="example">
+<node COLOR="#111111" CREATED="1301804930772" ID="ID_1562791517" MODIFIED="1390803183604" TEXT="single, double, trible"/>
+</node>
+</node>
+</node>
+</node>
+</node>
+<node COLOR="#990000" CREATED="1301804409128" ID="ID_501465181" MODIFIED="1390803183604" TEXT="Collective">
+<font NAME="SansSerif" SIZE="14"/>
+<node COLOR="#111111" CREATED="1301804970615" ID="ID_694889002" MODIFIED="1390803183604" TEXT="describe">
+<node COLOR="#111111" CREATED="1301804974535" ID="ID_222213530" MODIFIED="1390803183604" TEXT="groups/sets">
+<node COLOR="#111111" CREATED="1301804978497" ID="ID_1582767391" MODIFIED="1390803183604" TEXT="example">
+<node COLOR="#111111" CREATED="1301804981044" ID="ID_1560939753" MODIFIED="1390803183604" TEXT="pair, triad, dozen"/>
+</node>
+</node>
+</node>
+</node>
+<node COLOR="#990000" CREATED="1301804414829" ID="ID_1047477607" MODIFIED="1390803183604" TEXT="Distributive">
+<font NAME="SansSerif" SIZE="14"/>
+<node COLOR="#111111" CREATED="1301805030487" ID="ID_297988154" MODIFIED="1390803183605" TEXT="describe">
+<node COLOR="#111111" CREATED="1301805036525" ID="ID_541084165" MODIFIED="1390803183605" TEXT="alternating patterm">
+<node COLOR="#111111" CREATED="1301805047738" ID="ID_1141601408" MODIFIED="1390803183605" TEXT="example">
+<node COLOR="#111111" CREATED="1301805052686" ID="ID_703932730" MODIFIED="1390803183605" TEXT="every other person, every other week, every three women"/>
+</node>
+</node>
+</node>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1301802916742" HGAP="52" ID="ID_1524286626" MODIFIED="1390803183605" TEXT="number names" VSHIFT="-11">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1301805437551" ID="ID_423193930" MODIFIED="1390803183605" POSITION="left" TEXT="used by">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1301805446078" HGAP="25" ID="ID_377124272" MODIFIED="1390803183605" TEXT="operations" VSHIFT="12">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1301805486672" HGAP="19" ID="ID_653501803" MODIFIED="1390803183605" TEXT="study of">
+<font NAME="SansSerif" SIZE="14"/>
+<node COLOR="#111111" CREATED="1301805495406" HGAP="27" ID="ID_1810064550" MODIFIED="1390803183605" TEXT="arithmetic" VSHIFT="-33"/>
+</node>
+<node COLOR="#990000" CREATED="1301805533526" HGAP="22" ID="ID_222451913" MODIFIED="1390803183605" TEXT="number of inputs" VSHIFT="19">
+<font NAME="SansSerif" SIZE="14"/>
+<node COLOR="#111111" CREATED="1301805544768" ID="ID_878388907" MODIFIED="1390803183605" TEXT="(1) unary">
+<node COLOR="#111111" CREATED="1301805613877" ID="ID_1747971855" MODIFIED="1390803183605" TEXT="example">
+<node COLOR="#111111" CREATED="1301805617852" ID="ID_1740689439" MODIFIED="1390803183605" TEXT="successor (n=n+1), predecessor (n=n-1)"/>
+</node>
+</node>
+<node COLOR="#111111" CREATED="1301805551939" ID="ID_1369943603" MODIFIED="1390803183606" TEXT="(2) binary">
+<node COLOR="#111111" CREATED="1301805633620" ID="ID_1049660175" MODIFIED="1390803183606" TEXT="example">
+<node COLOR="#111111" CREATED="1301805637427" ID="ID_1103019470" MODIFIED="1390803183606" TEXT="addition, subtraction, multiplication, division, exponentian"/>
+</node>
+</node>
+<node COLOR="#111111" CREATED="1301805556869" ID="ID_693171126" MODIFIED="1390803183606" TEXT="(3) ternary"/>
+</node>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1301805820808" HGAP="15" ID="ID_611198203" MODIFIED="1390803183606" POSITION="right" TEXT="classification" VSHIFT="-56">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1301805854399" HGAP="32" ID="ID_1936717559" MODIFIED="1390803183606" TEXT="number systems" VSHIFT="40">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1301805965601" HGAP="23" ID="ID_808441576" MODIFIED="1390803183606" TEXT="sets" VSHIFT="35">
+<font NAME="SansSerif" SIZE="14"/>
+<node COLOR="#111111" CREATED="1301810051588" HGAP="17" ID="ID_1206080410" MODIFIED="1390803183606" TEXT="Natural numbers (&#x2115;)" VSHIFT="-11">
+<node COLOR="#111111" CREATED="1301811015657" HGAP="19" ID="ID_1427924385" MODIFIED="1390803183606" TEXT="purpose" VSHIFT="-4">
+<node COLOR="#111111" CREATED="1301810966903" ID="ID_1946306019" MODIFIED="1390803183606" TEXT="Counting - Cardinal"/>
+<node COLOR="#111111" CREATED="1301810973818" ID="ID_1471772482" MODIFIED="1390803183606" TEXT="Ordering - Ordinal"/>
+</node>
+<node COLOR="#111111" CREATED="1301811303463" ID="ID_921458590" MODIFIED="1390803183606" TEXT="study">
+<node COLOR="#111111" CREATED="1301811328365" HGAP="40" ID="ID_1546094148" MODIFIED="1390803183607" TEXT="Number Theory" VSHIFT="-4"/>
+<node COLOR="#111111" CREATED="1301811344846" HGAP="39" ID="ID_23437600" MODIFIED="1390803183607" TEXT="Combinatorics" VSHIFT="5"/>
+</node>
+</node>
+<node COLOR="#111111" CREATED="1301810093692" HGAP="23" ID="ID_1727453755" MODIFIED="1391518977479" VSHIFT="-15">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <font size="3">Integers (&#8484; = &#8484;<sup>+</sup>&#160;&#8746; 0 &#8746; &#8484;<sup>-</sup>)</font>
+    </p>
+  </body>
+</html></richcontent>
+<node COLOR="#111111" CREATED="1301805886769" HGAP="24" ID="ID_1602366133" MODIFIED="1390803183607" VSHIFT="-5">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Positive Integers (&#8484;<sup>+</sup>)
+    </p>
+  </body>
+</html></richcontent>
+<node COLOR="#111111" CREATED="1389177082728" ID="ID_1794692250" MODIFIED="1390803183607" TEXT="example">
+<node COLOR="#111111" CREATED="1389177096389" ID="ID_1557822302" MODIFIED="1390803183607" TEXT="(1, 2, 3 ...)"/>
+</node>
+</node>
+<node COLOR="#111111" CREATED="1301810500099" HGAP="23" ID="ID_1283703780" MODIFIED="1390803183608" TEXT="Zero (0)" VSHIFT="3"/>
+<node COLOR="#111111" CREATED="1301810478715" ID="ID_1939144134" MODIFIED="1390803183608">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Negative Integers (&#8484;<sup>-</sup>)
+    </p>
+  </body>
+</html></richcontent>
+<node COLOR="#111111" CREATED="1389177439730" ID="ID_1272931252" MODIFIED="1390803183608" TEXT="example">
+<node COLOR="#111111" CREATED="1389177448826" ID="ID_914582966" MODIFIED="1390803183608" TEXT="(-1, -2, -3 ...)"/>
+</node>
+</node>
+</node>
+<node COLOR="#111111" CREATED="1301805893809" ID="ID_115735426" MODIFIED="1391518423099" TEXT="Rational numbers (&#x211a;)">
+<node COLOR="#111111" CREATED="1323488891507" HGAP="19" ID="ID_1241639443" MODIFIED="1391519473060" TEXT="a/b and b &#x2260; 0." VSHIFT="-1"/>
+</node>
+<node COLOR="#111111" CREATED="1301805901881" ID="ID_762862044" MODIFIED="1391518407204" TEXT="Real numbers (&#x211d;)">
+<node COLOR="#111111" CREATED="1391518338287" ID="ID_286190622" MODIFIED="1391518889995" TEXT="value representing a quantity on real line (e.g. -5, &#x3c0;, &#x221a;2)">
+<node COLOR="#111111" CREATED="1391518397277" ID="ID_53810137" MODIFIED="1391518975403" TEXT="&#x211a; &#x2282; &#x211d;, &#x211a; &#x2282; &#x2115;, &#x211a; &#x2282; &#x2124;"/>
+</node>
+</node>
+<node COLOR="#111111" CREATED="1301805903672" ID="ID_470203872" MODIFIED="1390803183608" TEXT="Complex numbers (&#x2102;)"/>
+<node COLOR="#111111" CREATED="1301811116440" ID="ID_1314123806" MODIFIED="1390803183609" TEXT="Computable numbers"/>
+<node COLOR="#111111" CREATED="1391518541006" ID="ID_422789501" MODIFIED="1391518550887" TEXT="Irrational numbers">
+<node COLOR="#111111" CREATED="1391519038166" ID="ID_1339552136" MODIFIED="1391519431516" TEXT="real number that cannot be expressed as a ratio a/b, where a and b &#x2208; &#x2124; and b &#x2260; 0."/>
+</node>
+</node>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1390803190778" HGAP="32" ID="ID_966018866" MODIFIED="1390803270956" POSITION="left" TEXT="Stirling Numbers" VSHIFT="67">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+<attribute NAME="Named after" VALUE="James Stirling"/>
+<node COLOR="#00b439" CREATED="1390803274744" ID="ID_735221729" MODIFIED="1390803349693">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      First Kind (<font color="rgb(0, 0, 0)" size="13px" face="sans-serif"><i http-equiv="content-type" style="text-align: start; text-transform: none; text-indent: 0px; font-weight: normal; font-variant: normal; font-size: 13px; white-space: normal; line-height: 19.1875px; font-family: sans-serif; letter-spacing: normal; word-spacing: 0px; color: rgb(0, 0, 0); background-color: rgb(255, 255, 255)" content="text/html; charset=utf-8">s</i></font>)
+    </p>
+  </body>
+</html></richcontent>
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1390803289968" ID="ID_1269784654" MODIFIED="1390803321731" TEXT="Signed (can be negative)">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1390803361337" ID="ID_476729613" MODIFIED="1390803398694">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Second Kind (<font color="rgb(0, 0, 0)" size="13px" face="sans-serif"><i http-equiv="content-type" style="text-align: start; text-transform: none; text-indent: 0px; font-weight: normal; font-variant: normal; font-size: 13px; white-space: normal; line-height: 19.1875px; font-family: sans-serif; letter-spacing: normal; word-spacing: 0px; color: rgb(0, 0, 0); background-color: rgb(255, 255, 255)" content="text/html; charset=utf-8">S</i></font>)
+    </p>
+  </body>
+</html></richcontent>
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1390803405690" ID="ID_1093624243" MODIFIED="1390803417243" TEXT="Unsigned (never negative)">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+</node>
+</node>
+</map>
